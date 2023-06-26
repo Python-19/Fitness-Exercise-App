5505d3be7b2e4e868b66e06bce9452d058a10e58
Python-19 <mossaratyasmin@gmail.com>
2023-06-26 01:05:40 -0700

    commits

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': '36b6a5cb4emsh680cdf171693646p1faae5jsna0100fdff7c0',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 
